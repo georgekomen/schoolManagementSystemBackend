18ad3d7930b517f22ada637e67b2f393907e4a5a
KOMEN <g.komen@carepay.co.ke>
2018-07-15 12:31:57 +0300

    first forward

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM java:8
+VOLUME /tmp
+MAINTAINER George komen <gkkomensi@gmail.com>
+ADD build/libs/arafatproject-0.0.1-SNAPSHOT.jar app.jar
+ADD build/docker/libs /libs
+ADD build/classes/java/main /classes
+ADD build/resources/main /resources
+RUN bash -c 'touch /app.jar'
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
---
+++ d.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+./gradlew build
+docker build -t gcr.io/arafat-project/arafat-project .
+# docker login -u _json_key -p "$(cat /home/komen/.ssh/arafat-project-4094e61ad8c8.json)" https://gcr.io #only run once (the path is path to the credentials file -got from google IMA and Admin)
+docker push gcr.io/arafat-project/arafat-project
+ssh 35.237.90.117 docker pull gcr.io/arafat-project/arafat-project
+ssh 35.237.90.117 docker stop arafat-project
+ssh 35.237.90.117 docker rm arafat-project
+ssh 35.237.90.117 docker run -d -p 9000:5000 --name arafat-project --restart always gcr.io/arafat-project/arafat-project
