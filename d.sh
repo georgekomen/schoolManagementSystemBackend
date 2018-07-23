67bba29f570716a87365bcda42a37f8b90410115
KOMEN <g.komen@carepay.co.ke>
2018-07-23 23:39:52 +0300

    upadate docker building process

--- d.sh
+++ d.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-./gradlew build
-docker build -t gcr.io/arafat-project/arafat-project . --no-cache
-# docker login -u _json_key -p "$(cat /home/komen/.ssh/arafat-project-4094e61ad8c8.json)" https://gcr.io #only run once (the path is path to the credentials file -got from google IMA and Admin)
+./gradlew clean build
+docker build -t gcr.io/arafat-project/arafat-project .
+#docker login -u _json_key -p "$(cat /home/komen/Downloads/arafat-project-d69048874c78.json)" https://gcr.io #only run once (the path is path to the credentials file -got from google IMA and Admin)
 docker push gcr.io/arafat-project/arafat-project
 ssh 35.229.104.163 sudo docker pull gcr.io/arafat-project/arafat-project
 ssh 35.229.104.163 sudo docker stop arafat-project
