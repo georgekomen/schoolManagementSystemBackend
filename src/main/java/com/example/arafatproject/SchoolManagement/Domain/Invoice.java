f3d1b09cfbdc77ec4eb0da9757947e8e95eb70bf
KOMEN <g.komen@carepay.co.ke>
2018-09-20 21:59:35 +0300

    added invoice domain

---
+++ src/main/java/com/example/arafatproject/SchoolManagement/Domain/Invoice.java
@@ -0,0 +1,17 @@
+package com.example.arafatproject.SchoolManagement.Domain;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date date_created;
+    private Long course_id;
+    private Long invoice_amount;
+}
