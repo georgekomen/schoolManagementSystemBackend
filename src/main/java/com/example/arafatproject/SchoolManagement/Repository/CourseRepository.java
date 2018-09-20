a3e76fcbbb21be182c531b6032082cde61d2c5e6
KOMEN <g.komen@carepay.co.ke>
2018-09-20 22:20:49 +0300

    added repositories for all domains

---
+++ src/main/java/com/example/arafatproject/SchoolManagement/Repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.arafatproject.SchoolManagement.Repository;
+
+import com.example.arafatproject.SchoolManagement.Domain.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
