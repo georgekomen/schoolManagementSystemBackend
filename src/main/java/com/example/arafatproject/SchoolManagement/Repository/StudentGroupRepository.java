c0b41ca8ee22dfc94995cd739e63811a505a5df8
KOMEN <g.komen@carepay.co.ke>
2018-09-20 22:27:23 +0300

    fix issue

--- src/main/java/com/example/arafatproject/SchoolManagement/Repository/StudentGroupRepository.java
+++ src/main/java/com/example/arafatproject/SchoolManagement/Repository/StudentGroupRepository.java
@@ -1,8 +1,9 @@
 package com.example.arafatproject.SchoolManagement.Repository;
 
+import com.example.arafatproject.SchoolManagement.Domain.StudentGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentGroupRepository extends JpaRepository<StudentGroupRepository, Long> {
+public interface StudentGroupRepository extends JpaRepository<StudentGroup, Long> {
 }
