4164aa1df76a2c6bdde69255e8ca96069ddd0b6a
KOMEN <g.komen@carepay.co.ke>
2018-09-22 10:04:55 +0300

    added student payments when fetching student

--- src/main/java/com/example/arafatproject/SchoolManagement/Domain/Student.java
+++ src/main/java/com/example/arafatproject/SchoolManagement/Domain/Student.java
@@ -2,20 +2,22 @@ package com.example.arafatproject.SchoolManagement.Domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.lang.Nullable;
 
 @Entity
@@ -47,15 +49,8 @@ public class Student implements Serializable {
     private School school;
 
     @JsonView(View.StudentDetails.class)
-    public Double invoice() {
-        return 0D;
-    }
-
-    @JsonView(View.StudentDetails.class)
-    public Double payments() {
-        return 0D;
-    }
-
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "student")
+    private Set<StudentPayment> studentPayments = new HashSet<>();
 
     public Student(Date admission_date, String name, Gender gender, Long student_admission_number, School school) {
         this.admission_date = admission_date;
