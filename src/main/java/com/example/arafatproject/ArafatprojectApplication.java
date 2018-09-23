0f3bf90bbe33d1ca8dd00de8ce0ab6a711d01eec
KOMEN <g.komen@carepay.co.ke>
2018-09-23 16:39:35 +0300

    added cors filter

--- src/main/java/com/example/arafatproject/ArafatprojectApplication.java
+++ src/main/java/com/example/arafatproject/ArafatprojectApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -25,4 +28,18 @@ public class ArafatprojectApplication {
 		loggingFilter.setIncludePayload(true);
 		return loggingFilter;
 	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.addExposedHeader("X-Has-Next-Page");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
 }
